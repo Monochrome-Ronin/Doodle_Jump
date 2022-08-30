eedf184cae67a09701db5cd82a49b12c9fc18b98
Vlad <lavrishkovlad@gmail.com>
2022-08-30 22:29:48 +0300

    Add Controll position Horizontal

--- Assets/Scripts/Player/PlayerMover.cs
+++ Assets/Scripts/Player/PlayerMover.cs
@@ -6,22 +6,39 @@ using UnityEngine;
 public class PlayerMover : MonoBehaviour
 {
     [SerializeField] private SpriteRenderer _playerSprite;
-    [SerializeField] private float _speed = 6.5f;
+    [SerializeField] private float _speed = 7.5f;
     private Rigidbody2D _rigidbody2D;
     private float _horizontalDirectory;
     private void Start()
     {
         _rigidbody2D = GetComponent<Rigidbody2D>();
     }
+    private void Update()
+    {
+        ControllHorizontalPostion();
+
+    }
     private void FixedUpdate()
     {
 #if UNITY_ANDROID_API
+        MoveHorizontal();
+#endif
+    }
+
+    private void MoveHorizontal()
+    {
         _horizontalDirectory = Input.acceleration.x;
         if (_horizontalDirectory < 0)
             _playerSprite.flipX = true;
         else if (_horizontalDirectory > 0)
             _playerSprite.flipX = false;
         _rigidbody2D.velocity = new Vector2(_horizontalDirectory * _speed, _rigidbody2D.velocity.y);
-#endif
     }
+
+    private void ControllHorizontalPostion()
+    {
+        if (transform.position.x < -3) transform.position = new Vector2(2.7f, transform.position.y);
+        else if (transform.position.x > 3) transform.position = new Vector2(-2.7f, transform.position.y);
+    }
+
 }
