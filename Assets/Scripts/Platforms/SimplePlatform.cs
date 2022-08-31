542f3203884a4b43a8b04d472eea475599011170
Monochrome-Ronin <skryvulko@gmail.com>
2022-08-31 14:16:43 +0300

    fixed bug jumpforce

--- Assets/Scripts/Platforms/SimplePlatform.cs
+++ Assets/Scripts/Platforms/SimplePlatform.cs
@@ -5,13 +5,18 @@ using UnityEngine;
 public class SimplePlatform : BasePlatform
 {
     [SerializeField] private float _jumpForce = 6.5f;
+    private float _contactOffsetY = .69f;
 
     protected override void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.TryGetComponent(out Player player))
         {
-            if (collision.relativeVelocity.y > 0) return;
+            float offset = collision.transform.position.y - transform.position.y;
+            print(offset);
+            if (collision.relativeVelocity.y > 0 || Mathf.Abs(offset) < _contactOffsetY) return;
+            print("jump");
             Rigidbody2D rigidbody2D = player.GetComponent<Rigidbody2D>();
+            rigidbody2D.velocity = new Vector2(rigidbody2D.velocity.x, 0);
             rigidbody2D.AddForce(Vector2.up * _jumpForce, ForceMode2D.Impulse);
         }
     }
