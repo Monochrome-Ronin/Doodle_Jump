a3b02c414a5e11d2385f4f5a4113403ab522dad9
PaskevichMaksim <paskevichmaks@gmail.com>
2022-09-02 16:53:37 +0300

    Platform audio

--- Assets/Scripts/Environment/Platforms/SimplePlatform.cs
+++ Assets/Scripts/Environment/Platforms/SimplePlatform.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,6 +7,12 @@ public class SimplePlatform : Environments
 {
     [SerializeField] private float _jumpForce = 6.5f;
     private float _contactOffsetY = .69f;
+    private AudioSource _audioJump;
+
+    private void Awake()
+    {
+        _audioJump = GetComponent<AudioSource>();
+    }
 
     protected override void OnCollisionEnter2D(Collision2D collision)
     {
@@ -13,6 +20,7 @@ public class SimplePlatform : Environments
         {
             float offset = collision.transform.position.y - transform.position.y;
             if (collision.relativeVelocity.y > 0 || Mathf.Abs(offset) < _contactOffsetY) return;
+            _audioJump.Play();
             Rigidbody2D rigidbody2D = player.GetComponent<Rigidbody2D>();
             rigidbody2D.velocity = new Vector2(rigidbody2D.velocity.x, 0);
             rigidbody2D.AddForce(Vector2.up * _jumpForce, ForceMode2D.Impulse);
