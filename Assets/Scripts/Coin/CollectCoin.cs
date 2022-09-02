1799260f978df1185c7c5c9670e1a1b741f0870a
PaskevichMaksim <paskevichmaks@gmail.com>
2022-09-02 15:04:06 +0300

    Coin sound

--- Assets/Scripts/Coin/CollectCoin.cs
+++ Assets/Scripts/Coin/CollectCoin.cs
@@ -6,16 +6,19 @@ using UnityEngine;
 public class CollectCoin : MonoBehaviour
 {
     public int _coin;
+    private AudioSource _audioCoin;
 
     private void Start()
     {
         _coin = 0;
+        _audioCoin = GetComponent<AudioSource>();
     }
 
     private void OnTriggerEnter2D(Collider2D collider2D)
     {
         if (collider2D.gameObject.CompareTag("Player"))
         {
+            _audioCoin.Play();
             Destroy(gameObject);
             _coin ++;
         }
