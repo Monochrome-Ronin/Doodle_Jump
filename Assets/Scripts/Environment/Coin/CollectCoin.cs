860b9687368cd0c8b791a9ee909593988a2869fa
PaskevichMaksim <paskevichmaks@gmail.com>
2022-09-02 16:14:06 +0300

    Audio

--- Assets/Scripts/Environment/Coin/CollectCoin.cs
+++ Assets/Scripts/Environment/Coin/CollectCoin.cs
@@ -5,12 +5,37 @@ using UnityEngine;
 
 public class CollectCoin : Environments
 {
+    private AudioSource _audioCoin;
+
+    private void Awake()
+    {
+        _audioCoin = GetComponent<AudioSource>();
+    }
+
+    private void Start()
+    {
+        ClampPosition();
+    }
+
     private void OnTriggerEnter2D(Collider2D collider2D)
     {
         if (collider2D.gameObject.CompareTag("Player"))
         {
+            _audioCoin.Play();
             Saver.SaverIntPrefs("Coins", Saver.GetIntPrefs("Coins") + 1);
-            Destroy(gameObject);
+            Destroy(gameObject, 0.1f);
+        }
+    }
+
+    private void ClampPosition()
+    {
+        if (transform.position.x < -2.5f)
+        {
+            transform.position = new Vector3(-2.5f, transform.position.y, 0);
+        }
+        else if (transform.position.x > 2.5f)
+        {
+            transform.position = new Vector3(2.5f, transform.position.y, 0);
         }
     }
 }
--- Assets/Scripts/Environment/Other/Spawner.cs
+++ Assets/Scripts/Environment/Other/Spawner.cs
@@ -2,25 +2,20 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 public class Spawner : MonoBehaviour
 {
-    [SerializeField] private int[] _chanceOfPlatrofm;
-    [SerializeField] private Environments[] _platforms;
+    [SerializeField] private int[] _chanceOfEnviroments;
+    [SerializeField] private Environments[] _environments;
     [SerializeField] private Transform _topBound;
-    [SerializeField] private GameObject _coin;
-    Environments _lastPlatform;
+    [SerializeField] Environments _lastPlatform;
     private void FixedUpdate()
     {
-        if (_lastPlatform == null)
+        if (_lastPlatform.transform.position.y < _topBound.position.y)
         {
             SpawnPlatform();
         }
-        else if (_lastPlatform.transform.position.y < _topBound.position.y)
-        {
-            SpawnPlatform();
-        }
-        
     }
 
     private void SpawnPlatform()
@@ -36,45 +31,38 @@ public class Spawner : MonoBehaviour
             spawnPostion.x = 0;
         }
 
-        _lastPlatform = Instantiate(RandomPlatform(), spawnPostion, Quaternion.identity);
+        _lastPlatform = Instantiate(RandomSpawn(), spawnPostion, Quaternion.identity);
         
         
     }
 
-    void SpawnCoin()
+   /*private void SpawnCoin()
     {
         int _randomNumber = Random.Range(0, 10);
         if (_randomNumber > 6)
         {
-            GameObject coin = Instantiate(_coin,
+            CollectCoin coin = Instantiate(_coin,
                 new Vector3(_lastPlatform.transform.position.x + Random.Range(-1, 1),
                     _lastPlatform.transform.position.y + Random.Range(1f, 2.5f), 0), _coin.transform.rotation, transform);
-            if (coin.transform.position.x < -2.5f)
-            {
-                coin.transform.position = new Vector3(-2.5f, _coin.transform.position.y, 0);
-            }
-            else if (_coin.transform.position.x > 2.5f)
-            {
-                coin.transform.position = new Vector3(2.5f, _coin.transform.position.y, 0);
-            }
+            
 
         }
-    }
+    }*/
 
-    Environments RandomPlatform()
+    Environments RandomSpawn()
     {
         int maxValue = 0;
         int currentPlatform = 0;
-        int[] randomArr = new int[_platforms.Length];
-        for (int i = 0; i < _platforms.Length; i++)
+        int[] randomArr = new int[_environments.Length];
+        for (int i = 0; i < _environments.Length; i++)
         {
-            randomArr[i] = Random.Range(0, _chanceOfPlatrofm[i] + 1);
+            randomArr[i] = Random.Range(0, _chanceOfEnviroments[i] + 1);
             if (maxValue < randomArr[i])
             {
                 maxValue = randomArr[i];
                 currentPlatform = i;
             }
         }
-        return _platforms[currentPlatform];
+        return _environments[currentPlatform];
     }
 }
