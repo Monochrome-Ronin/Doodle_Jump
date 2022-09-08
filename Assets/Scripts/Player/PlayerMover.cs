8adae2b68f52758c424a751b453cc227db8291f3
Vlad <lavrishkovlad@gmail.com>
2022-09-08 20:52:38 +0300

    Camera Controll Width

--- Assets/Scripts/Player/PlayerMover.cs
+++ Assets/Scripts/Player/PlayerMover.cs
@@ -17,12 +17,19 @@ public class PlayerMover : MonoBehaviour
     [SerializeField] private Image _playerImage;
     private Rigidbody2D _rigidbody2D;
     private float _horizontalDirectory;
+    private bool _isPhone;
 
     public Sprite JumpSprite { get => _jumpSprite; set => _jumpSprite = value; }
     public Sprite IdleSprite { get => _idleSprite; set => _idleSprite = value; }
 
     private void Start()
     {
+#if UNITY_ANDROID_API
+        _isPhone = true;
+#endif
+#if UNITY_EDITOR
+        _isPhone = false;
+#endif
         _rigidbody2D = GetComponent<Rigidbody2D>();
         if (_playerSpriteRenderer != null) _playerSpriteRenderer.sprite = _idleSprite;
         else _playerImage.sprite = _idleSprite;
@@ -35,12 +42,8 @@ public class PlayerMover : MonoBehaviour
     private void FixedUpdate()
     {
         if (_isMover == false) return;
-#if UNITY_ANDROID_API
-        _horizontalDirectory = Input.acceleration.x;
-#endif
-#if UNITY_EDITOR
-        _horizontalDirectory = Input.GetAxis("Horizontal");
-#endif
+        if (_isPhone) _horizontalDirectory = Input.acceleration.x;
+        else if (!_isPhone) _horizontalDirectory = Input.GetAxis("Horizontal");
         MoveHorizontal();
     }
     private void MoveHorizontal()
@@ -49,12 +52,12 @@ public class PlayerMover : MonoBehaviour
             _playerSpriteRenderer.flipX = true;
         else if (_horizontalDirectory > 0 && _playerSpriteRenderer != null)
             _playerSpriteRenderer.flipX = false;
-        _rigidbody2D.velocity = new Vector2(_horizontalDirectory * _speed, _rigidbody2D.velocity.y);
+        _rigidbody2D.velocity = new Vector3(_horizontalDirectory * _speed, _rigidbody2D.velocity.y, 0);
     }
     private void ControllHorizontalPostion()
     {
-        if (transform.position.x < -3) transform.position = new Vector2(2.7f, transform.position.y);
-        else if (transform.position.x > 3) transform.position = new Vector2(-2.7f, transform.position.y);
+        if (transform.position.x < -3) transform.position = new Vector3(2.7f, transform.position.y, -1);
+        else if (transform.position.x > 3) transform.position = new Vector3(-2.7f, transform.position.y, -1);
     }
     public void JumpAnim()
     {
